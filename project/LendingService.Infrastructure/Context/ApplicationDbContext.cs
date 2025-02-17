e4994beb87c5e0d463ce7ae90fcd83b016dceedc
Fernando Gil <fernandogilvidal@gmail.com>
2025-02-17 20:34:06 +0100

    Add infrastructure

---
+++ project/LendingService.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LendingService.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace LendingService.Infrastructure.Context
+{
+    internal class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+    }
+}
---
+++ project/LendingService.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,19 @@
+﻿using LendingService.Core.Ports;
+using LendingService.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LendingService.Infrastructure
+{
+    public static  class InfrastructureExtensions
+    {
+        public static void AddPersistence(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("LedingServiceInMemoryDb"));
+
+
+            serviceCollection.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        }
+    }
+}
